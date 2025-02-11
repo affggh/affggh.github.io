24d6de9ed8b194e558cb6e1a9a92991613d202ff
affggh <879632264@qq.com>
2025-02-11 20:53:42 +0800

    Site updated: 2025-02-11 20:53:41

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mainbody = document.getElementById("main-body");
+var darkmod = 0;
+add_pic = function () {var imgs=document.getElementsByTagName('img');
+for (var i=0; i<imgs.length; i++){
+    imgs[i].classList.add('mdui-img-fluid')
+};};
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+    mainbody.classList.add("mdui-theme-layout-dark");
+    darkmod = 1
+}
+function darkmod_use(){
+    if (darkmod == 1){mainbody.classList.remove("mdui-theme-layout-dark");
+    darkmod = 0;} else {
+        mainbody.classList.add("mdui-theme-layout-dark");
+        darkmod = 1;}};
